b060a7233ad1d4610e78df735914aea4fb146a5e
egber2708 <egber27@hotmail.com>
2019-08-02 15:50:21 -0500

    Configuro Test de prueba -green

---
+++ tests/Feature/PruebaTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace egber\Press\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class PruebaTest extends TestCase
+{
+    /**@test */
+    public function test_prueba(){
+        $this->assertTrue(true);
+    }
+    
+}
